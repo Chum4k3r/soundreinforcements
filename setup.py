6b10522f5042ef4a3f1aadd30c95489aa8990a67
Chum4k3r <joao.paes@eac.ufsm.br>
2020-08-03 16:16:49 -0300

    [INTERNAL] Added setup file.

---
+++ setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Setup file
+
+author: João Vitor Gutkoski Paes
+"""
+
+from setuptools import setup
+
+setup(name='Sound Reinforcement Systems',
+      version='0.1.0a',
+      description='Modelling of SRS and sound propagation.',
+      url='https://github.com/Chum4k3r/soundreinforcements.git',
+      author='João Vitor Gutkoski Paes',
+      author_email='joaovitorgpaes@gmail.com',
+      license='MIT',
+      install_requires=['numpy'],
+      packages=['soundreinforcements'],
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent"
+          ])
+          
