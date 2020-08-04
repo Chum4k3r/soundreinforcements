7123d665db70862ed8881ed23019c99bf8a046fd
Chum4k3r <joao.paes@eac.ufsm.br>
2020-08-04 00:31:01 -0300

    [DOCS] Added dimension units as comments for each parameter.

--- example.py
+++ example.py
@@ -10,16 +10,26 @@ import numpy as np
 import soundreinforcements as srs
 
 # Frequencies of analysis.
-freqs = np.array([125, 250, 500, 1000, 2000, 4000], dtype='float32')
+freqs = np.array([125, 250, 500, 1000, 2000, 4000], dtype='float32')  # Hz
 
 # Source power level for each frequency
-powerlevel = np.array(freqs.size * [100])
+powerlevel = np.array(freqs.size * [100])  # dB ref 1e-12 W
 
 # Air properties abstraction, for sound attenuation by air absorption.
-air = srs.Air(temp=23.2, hum=66.5, atm=101310, freqs=freqs)
+air = srs.Air(temp=23.2,  # °C
+              hum=66.5,   # %
+              atm=101310, # Pa
+              freqs=freqs)
 
 # A line source placed at (x=1.5, y=1.0, z=2.8) m and oriented to (nx=0, ny=1, nz=0)
-src = srs.Source(pos=[1.5, 1., 2.8], ori=[0., 1., 0.], swl=powerlevel, directivity=0.5)
+src = srs.Source(pos=[1.5,    # m
+                      1.,     # m
+                      2.8],   # m
+                 ori=[0.,     # m
+                      1.,     # m
+                      0.],    # m
+                 swl=powerlevel,
+                 directivity=0.5)  # dimensionless
 
 # Receivers, placed a few meters away from source, oriented towards the source, immersed in air
 recs = []
@@ -32,3 +42,5 @@ for num, rec in enumerate(recs):
     print(f"Frequencies: {freqs} Hz")
     print(f"SPL: {rec.spl_from_source(src)} dB ref 2e-5 Pa")
     print()
+    
+    
